5f6733ba9b7a434ad211128f248e00c1d2d14540
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2020-12-15 13:06:46 +0200

    Добавьте файлы проекта.

---
+++ Deimos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string i = new string(new char[] { '2', 'a', 'g', '8', 's', 'h', '4', '0', 'j', '1' });
+                Console.WriteLine(i.Replace("2", " ").Replace("2", " ").Replace ("a", " ").Replace ("8", " ").Replace ("s", " ").Replace ("0", " "));
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
